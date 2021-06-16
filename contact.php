8d6ab45b432c706c3c16d0dd35f5c8302f2ab843
Deepak Ugale <ugale.deepak3010@gmail.com>
2021-06-16 07:37:14 +0530

    Revert "Merge pull request #11 from NishitaDeglur/patch-4"
    This reverts commit 741e7e8a0f6bdbea4e608c24fb7e891e1d80dd83, reversing
    changes made to 1b5f55a92ec71b71a67263a12d7952f74135fdcd.

--- contact.php
+++ contact.php
@@ -1,56 +1,9 @@
+<!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title> Contaact US Page</title>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity "sha384-wvfxpqpZZVQGK6TAh5PVLGOfQNHSoD2xbE+QKPxCAFINEevoEH3SI0sibVc0QVnN" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href ="style.css"> 
+	<title>Contact</title>
 </head>
 <body>
-	<section class="contact">
-	<div class="contact">
-		<h2>Contact Us</h2>
-		<p>Any Query please contact us</p>
-	</div>
-	<div class="container">
-		<div class="contactInfo">
-			<div class="box">
-				<div class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
-				<div class="text">
-				<h3> Address</h3>
-				<p> cidco,<br>Aurangabad,<br>431001</p>
-				</div>
-			<div class="box">
-				<div class="icon"><i class="fa fa-phone" aria-hidden="true"></i></div>
-				<div class="text">
-				<h3> Phone</h3>
-				<p>8975621043<br>8459721313<br>9767928537</p>
-				</div>
-			<div class="box">
-				<div class="icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></div>
-				<div class="text">
-				<h3> Email</h3>
-				<p> shopperwings@gmail.com</p>
-				</div>
-			<div class="contactForm">
-			<form>
-				<h2>Send Message</h2>
-				<div class="inputBox">
-					<input type="text" name="" required="required">
-					<span>Full Name</span>
-				</div>
-				<div class="inputBox">
-					<input type="text" name="" required="required">
-					<span>Email</span>
-				</div>
-				<div class="inputBox">
-					 <textarea required="required"></textarea>
-					<span>Type your Message..
-					
-					</span>
-				</div>
-				<div class="inputBox">
-					 <input type="submit" name="" value="Send">
-				</div>
-	</section>
+	<h1>This page show our team contact details</h1>
 </body>
 </html>
